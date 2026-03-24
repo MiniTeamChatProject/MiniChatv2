dd939c452b9e48b288955f00816ad332ec7054fe
nameisname <name_is_name@qq.com>
2026-03-24 16:08:25 +0800

    feat: 添加 Swagger UI 支持
    - 集成 github.com/go-kratos/swagger-api/openapiv2
    - 在 /q/ 路径提供 Swagger UI 界面
    - 可通过浏览器直接访问和测试 API
    - 支持服务选择和方法查看
    
    访问地址: http://localhost:8000/q/

--- service/room/internal/server/http.go
+++ service/room/internal/server/http.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/go-kratos/swagger-api/openapiv2"
 )
 
 // NewHTTPServer new a HTTP server.
@@ -28,6 +29,12 @@ func NewHTTPServer(c *conf.Server, roomSvc *service.RoomService, logger log.Logg
 	}
 	srv := http.NewServer(opts...)
 	v1.RegisterRoomServiceHTTPServer(srv, roomSvc)
+
+	// 添加 Swagger UI
+	if c.Http.Metadata {
+		h := openapiv2.NewHandler()
+		srv.HandlePrefix("/q/", h)
+	}
 
 	return srv
 }
